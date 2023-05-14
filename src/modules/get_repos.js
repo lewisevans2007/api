aaa15d582c6a05d34605296fa58132d3ff087989
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-05-14 08:11:38 +0100

    Update comments
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- src/modules/get_repos.js
+++ src/modules/get_repos.js
@@ -1,7 +1,7 @@
 /* API
  * A api for my projects and other things
  * Github: https://www.github.com/awesomelewis2007/api
- * Licence: GPL-3.0
+ * Licence: GNU General Public License v3.0
  * By: Lewis Evans
 */
 
